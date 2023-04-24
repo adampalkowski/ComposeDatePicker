e8a9d4f7cfc07ee396452810625ef176fa2ddce0
Adam <adampalkowski6@gmail.com>
2023-04-25 01:16:43 +0200

    initial commit

---
+++ app/src/main/java/com/example/customdatepickercompose/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.customdatepickercompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.customdatepickercompose.ui.theme.CustomDatePickerComposeTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalComposeUiApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CustomDatePickerComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    Column() {
+                        DatePickerTimeline(onDateSelected = { date -> })
+                        HorizontalDatePicker()
+                    }
+                }
+            }
+        }
+    }
+}
+
+
